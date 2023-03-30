5a75ccc3b0adeff502dfb27bb1dcd7b15eb825f2
0xcryptolub <blackhat1523@gmail.com>
2023-03-30 20:42:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const TodoApp = () => {
+  const [todos, setTodos] = useState(() => {
+    const storedTodos = localStorage.getItem('todos');
+    return storedTodos ? JSON.parse(storedTodos) : [];
+  });
+  const [newTodo, setNewTodo] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodoChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleNewTodoSubmit = (event) => {
+    event.preventDefault();
+    if (!newTodo.trim()) { 
+      return;
+    }
+    const newTodoItem = {
+      id: Date.now(),
+      title: newTodo,
+    };
+    setTodos([...todos, newTodoItem]);
+    setNewTodo('');
+  };
+
+  const handleTodoDelete = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  const handleTodoClick = (id) => {
+    handleTodoDelete(id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='main'>
+      <form onSubmit={handleNewTodoSubmit}>
+        <p for="input">New Task :</p>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={handleNewTodoChange}
+          autoFocus
+        />
+        <button type="submit">Add Task</button>
+      </form>
+        {todos.map((todo) => (
+          <label
+            key={todo.id}
+            htmlFor={`todo-${todo.id}`}
+            style={{ backgroundColor: 'white', display: 'block', padding: '10px', margin: '5px', borderRadius: '5px' }}
+            onMouseEnter={() => {
+              document.getElementById(`todo-${todo.id}`).style.color = 'red';
+            }}
+            onMouseLeave={() => {
+              document.getElementById(`todo-${todo.id}`).style.color = 'black';
+            }}
+            onClick={() => handleTodoClick(todo.id)}
+          >
+            <span id={`todo-${todo.id}`}>{todo.title}</span>
+          </label>
+        ))}
     </div>
   );
-}
+};
 
-export default App;
+export default TodoApp;
